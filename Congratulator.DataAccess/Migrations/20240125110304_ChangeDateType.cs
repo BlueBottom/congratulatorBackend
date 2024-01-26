c584a77b6483825c1afff60bed1da7419b2a1a66
BlueBottom <xxdelfinchikxx@gmail.com>
2024-01-26 19:56:54 +0300

    Added class library for data

---
+++ Congratulator.DataAccess/Migrations/20240125110304_ChangeDateType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Congratulator.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDateType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Birthdays",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Date",
+                table: "Birthdays",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
